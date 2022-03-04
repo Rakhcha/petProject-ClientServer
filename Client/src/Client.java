894b7d2a5bc9c50e8ff7ce723b9444673de12beb
Rakhchik <ilyachannle@gmail.com>
2022-03-05 01:04:52 +0300

    Initial commit

---
+++ Client/src/Client.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket("127.0.0.1", 8000);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ){
+
+            System.out.println("Connection created");
+            String request = "Ilya Rakhcheev";
+            System.out.println("Request: " + request);
+
+            writer.write(request);
+            writer.newLine();
+            writer.flush();
+
+            String response = reader.readLine();
+            System.out.println("Response: " + response);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
